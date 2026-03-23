9d8383b1aeefd859989ffefc25824cc1bfd197a7
clallow <1469592388@qq.com>
2026-03-23 14:27:18 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
-importimport { defineConfig } from 'vite' 可以翻译为：
-
-```javascript
-import { defineConfig } from 'vite'；
- { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({默认导出 defineConfig({
-  plugins: [react()],   插件:反应(),
-  base: '/curriculumvitae/',
+export default defineConfig({
+  plugins: [react()],
+  base: '/curriculumvitae/',   // 注意末尾斜杠不能少
 })
